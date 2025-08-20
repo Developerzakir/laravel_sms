fa4e7bf9667e96eea25e8f5f80977085816023a1
Developerzakir <zakir1234@!za>
2025-08-20 19:50:27 +0600

    student id card generate

--- resources/views/admin/inc/sidebar.blade.php
+++ resources/views/admin/inc/sidebar.blade.php
@@ -173,6 +173,9 @@
 
               <li class="{{ ($route == 'student.result.view')?'active':'' }}"><a href="{{ route('student.result.view') }}"><i class="ti-more"></i>Student Result </a></li>
 
+                <li class="{{ ($route == 'student.idcard.view')?'active':'' }}"><a href="{{ route('student.idcard.view') }}"><i class="ti-more"></i>Student ID Card </a></li>    
+
+
           </ul>
         </li>
 		  
